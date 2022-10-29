3afbde08c439440818ca315247cd7bff89a6aa67
sumi-0011 <selina2000@naver.com>
2022-10-29 23:44:02 +0900

    [problem7] feat : 친구 관계 그래프를 나타내는 객체 찾는 메소드 정의

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,16 @@
+const getRelationPeople = (friends) => {
+  const obj = {};
+
+  for (const [f1, f2] of friends) {
+    f1 in obj ? (obj[f1] = [...obj[f1], f2]) : (obj[f1] = [f2]);
+    f2 in obj ? (obj[f2] = [...obj[f2], f1]) : (obj[f2] = [f1]);
+  }
+
+  return obj;
+};
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const relations = getRelationPeople(friends);
 }
 
 module.exports = problem7;
