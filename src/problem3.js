921aab37bb4d7df365bd2f5d8bae70ca6b118800
sumi-0011 <selina2000@naver.com>
2022-10-29 17:44:19 +0900

    [problem3] feat : 손뼉을 쳐야하는 횟수 count

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
 const getNumberToCharList = (number) => {
   return number.toString().split("");
 };
+
+const get369Count = (arr) => {
+  return arr.filter((c) => c === "3" || c === "6" || c === "9").length;
+};
+
 function problem3(number) {
   var answer;
   return answer;
