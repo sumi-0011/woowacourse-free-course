0ee66a5530de7e8a83e50e81b9efca7b9e57c99f
sumi-0011 <selina2000@naver.com>
2022-10-29 23:12:05 +0900

    [problem5] feat : 해당하는 지폐 or 동전으로 변환할수 있는 최대개수를 반환, 그리고 변환된 후  돈의 나머지를 반환하는 메소드 정의

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,13 @@
+const BILL = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+const getMaxUnitCnt = (unit, money) => {
+  return parseInt(money / unit);
+};
+
+const getUnitRest = (unit, money) => {
+  return money % unit;
+};
+
 function problem5(money) {
   var answer;
   return answer;
