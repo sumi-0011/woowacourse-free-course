48c37ff9bbfa8b5ab30afee2e0d2d7a7aae37a0d
sumi-0011 <selina2000@naver.com>
2022-10-29 16:32:40 +0900

    [problem1] feat : n자리 숫자를 1자리 숫자 리스트로 분할하는 메소드 생성, 숫자 분할

--- src/problem1.js
+++ src/problem1.js
@@ -1,10 +1,18 @@
 function checkSuccessfulOpenPage(leftPage, rightPage) {
   return rightPage - leftPage !== 1;
 }
+
+function getNumberList(number) {
+  return [...number.toString()].map((s) => parseInt(s));
+}
 function problem1(pobi, crong) {
   if (checkSuccessfulOpenPage(...pobi) || checkSuccessfulOpenPage(...crong)) {
     return -1;
   }
+
+  const pobiNumbers = pobi.map((po) => getNumberList(po));
+  const crongNumbers = crong.map((cr) => getNumberList(cr));
+
 }
 
 module.exports = problem1;
