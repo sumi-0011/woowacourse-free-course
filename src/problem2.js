2ce5d8df15a15072d7c02a19b89d8b698cca2544
sumi-0011 <selina2000@naver.com>
2022-10-29 17:23:35 +0900

    [problem2] feat : 연속하는 중복 문자들을 삭제

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [""];
+  let idx = 0;
+
+  for (const c of cryptogram) {
+    if (c === stack[idx]) {
+      stack.pop();
+      idx--;
+    } else {
+      stack.push(c);
+      idx++;
+    }
+  }
+
+  return stack.join("");
 }
 
 module.exports = problem2;
