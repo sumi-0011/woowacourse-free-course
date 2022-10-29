6038bfb804c5d0e43a9739c2743e3f786f484d65
sumi-0011 <selina2000@naver.com>
2022-10-29 16:41:09 +0900

    [problem1] feat : 본인의 가장 큰 점수 구하기
    더하기, 곱하기를 하는 함수를 구현하고, 그 함수를 이용하여 가장 큰 점수를 구함

--- src/problem1.js
+++ src/problem1.js
@@ -5,6 +5,32 @@ function checkSuccessfulOpenPage(leftPage, rightPage) {
 function getNumberList(number) {
   return [...number.toString()].map((s) => parseInt(s));
 }
+
+const getPlus = (numbers) => {
+  const result = numbers.reduce(function add(sum, currValue) {
+    return sum + currValue;
+  }, 0);
+  return result;
+};
+
+const getMultiple = (numbers) => {
+  const result = numbers.reduce(function multi(res, currValue) {
+    return res * currValue;
+  }, 1);
+  return result;
+};
+
+const getMaxNumber = (personNumberList) => {
+  const calcRes = [];
+
+  personNumberList.forEach((numberList) => {
+    calcRes.push(getPlus(numberList));
+    calcRes.push(getMultiple(numberList));
+  });
+
+  return Math.max(...calcRes);
+};
+
 function problem1(pobi, crong) {
   if (checkSuccessfulOpenPage(...pobi) || checkSuccessfulOpenPage(...crong)) {
     return -1;
@@ -12,6 +38,9 @@ function problem1(pobi, crong) {
 
   const pobiNumbers = pobi.map((po) => getNumberList(po));
   const crongNumbers = crong.map((cr) => getNumberList(cr));
+
+  const pobiMax = getMaxNumber(pobiNumbers);
+  const crongMax = getMaxNumber(crongNumbers);
 
 }
 
