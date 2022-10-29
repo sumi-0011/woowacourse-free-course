03a6d3dbb7ad84a0fcd8649da3a3b9c4477d4230
sumi-0011 <selina2000@naver.com>
2022-10-29 23:07:07 +0900

    [problem4] feat : 문자가 대문자인지 확인하는 메소드 정의

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,14 @@
+const UPPER_FIRST_CODE = "A".charCodeAt(0);
+const UPPER_LAST_CODE = "Z".charCodeAt(0);
+const LOWER_FIRST_CODE = "a".charCodeAt(0);
+const LOWER_LAST_CODE = "z".charCodeAt(0);
+
+const checkUpperCase = (char) => {
+  return (
+    char.charCodeAt(0) >= UPPER_FIRST_CODE &&
+    char.charCodeAt(0) <= UPPER_LAST_CODE
+  );
+};
 function problem4(word) {
   var answer;
   return answer;
