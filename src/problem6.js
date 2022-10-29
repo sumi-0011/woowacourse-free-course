bdc61c40298dda2726100ce4bde36d2903dfefd7
sumi-0011 <selina2000@naver.com>
2022-10-29 23:23:18 +0900

    [problem6] feat : 닉네임을 두문자 단위로 분리, 분리된 두문자에 해당하는 이메일 체크

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,29 @@
+const seperateName = (name) => {
+  const arr = [];
+
+  for (let i = 0; i < name.length - 1; i++) {
+    arr.push(name[i] + name[i + 1]);
+  }
+
+  return arr;
+};
+
+const getSeperateNameObj = (forms) => {
+  const obj = {};
+
+  for (const person of forms) {
+    const [email, name] = person;
+    const names = seperateName(name);
+
+    names.forEach((name) => {
+      name in obj ? (obj[name] = [...obj[name], email]) : (obj[name] = [email]);
+    });
+  }
+
+  return obj;
+};
 function problem6(forms) {
-  var answer;
-  return answer;
+  const seperateNameObj = getSeperateNameObj(forms);
 }
 
 module.exports = problem6;
