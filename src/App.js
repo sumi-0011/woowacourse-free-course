cbd3204bcaff34810d0a5f1da157f994cf166f9c
sumi-0011 <selina2000@naver.com>
2022-11-06 21:46:03 +0900

    feat: 컴퓨터가 랜덤한 3개의 숫자를 생성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
+  constructor() {
+    this.computer = {
+      randomNumbers: [],
+    };
+  }
 
   startGame() {
     Console.print("숫자 야구 게임을 시작합니다.");
 
+    this.setRandomNumbers();
+
     Console.close();
   }
+
   play() {
     this.startGame();
+
     return;
+  }
+
+  setRandomNumbers() {
+    const randomNumbers = this.getRandomNumbers(3);
+    this.computer = {
+      randomNumbers,
+    };
+  }
+
+  getRandomNumbers(cnt) {
+    const numbers = [];
+    for (let i = 0; i < cnt; i++) {
+      let random = 0;
+      do {
+        random = Random.pickNumberInRange(1, 9);
+      } while (numbers.includes(random));
+      numbers.push(random);
+    }
+
+    return numbers;
   }
 }
 
