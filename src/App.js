083d5711330ae9634c5c799baf0e5502380b2045
sumi-0011 <selina2000@naver.com>
2022-11-06 21:21:42 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 class App {
-  play() {}
+
+  startGame() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+
+    Console.close();
+  }
+  play() {
+    this.startGame();
+    return;
+  }
 }
 
 module.exports = App;
