1c19e8bf73d0f724363935baa8c1e55e5f013bf2
sumi-0011 <selina2000@naver.com>
2022-11-12 21:10:53 +0900

    feat: 입력금액에 따른 로또 수량 체크

--- src/LottoGame.js
+++ src/LottoGame.js
@@ -1,5 +1,8 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { validInteger, validThousandWonUnit } = require('./validation');
+const { getRandomNumbers, calcPortion } = require('./utils');
+const LOTTO_PRICE = 1000;
+
 class LottoGame {
   constructor() {}
 
@@ -7,7 +10,11 @@ class LottoGame {
     this.print('구입금액을 입력해 주세요.', (answer) => {
       validInteger(answer);
       validThousandWonUnit(answer);
+      const money = parseInt(answer);
 
+      const lottoCount = calcPortion(money, LOTTO_PRICE);
+
+      console.log('lottoCount: ', lottoCount);
       this.exit();
     });
   }
---
+++ src/utils.js
@@ -0,0 +1,49 @@
+const { Random } = require('@woowacourse/mission-utils');
+const { CONVERT_TO_NUMBER_FAIL, NOT_INTEGER } = require('../src/errorMessage');
+
+/**
+ *  minBound ~ maxBound 사이의 n개의 서로 다른 랜덤한 숫자 생성
+ * @param {number} n
+ * @param {number} minBound
+ * @param {number} maxBound
+ * @returns {number[]}
+ */
+const getRandomNumbers = (n, minBound = 1, maxBound = 9) => {
+  const randomNumbers = [];
+
+  while (randomNumbers.length < n) {
+    let random = Random.pickNumberInRange(minBound, maxBound);
+    if (!randomNumbers.includes(random)) {
+      randomNumbers.push(random);
+    }
+  }
+  return randomNumbers;
+};
+
+/**
+ * @param {string} str
+ * @returns {number}
+ */
+const convertStringToNumber = (str) => {
+  const res = Number(str);
+
+  if (Number.isNaN(res)) {
+    throw new Error(CONVERT_TO_NUMBER_FAIL);
+  }
+  return res;
+};
+
+/**
+ * 몫 계산
+ * @param {number} number
+ * @param {number} divider
+ * @returns
+ */
+const calcPortion = (number, divider) => {
+  return parseInt(number / divider);
+};
+
+module.exports = {
+  getRandomNumbers,
+  calcPortion,
+};
